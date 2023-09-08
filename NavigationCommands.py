5837c64ab97ed6109dc9b822845ceaf35e53ea02
RmdGroupProjects <uramudu666@gmail.com>
2023-09-08 10:37:14 +0530

    NavigationCommands

--- NavigationCommands.py
+++ NavigationCommands.py
@@ -1,11 +1,17 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 drivers=webdriver.Chrome(executable_path=r"C:\Users\ram\Desktop\Drivers\chromedriver.exe")
 drivers.maximize_window()
 drivers.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-title=drivers.title
-print(title)
-url=drivers.current_url
-print(url)
-drivers.close()
+print(drivers.title)
+time.sleep(6)
+drivers.get("https://www.facebook.com/login.php/")
+print(drivers.title)
+time.sleep(6)
+drivers.back()
+print(drivers.title)
+time.sleep(6)
+drivers.forward()
+print(drivers.title)
