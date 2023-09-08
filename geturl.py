d83687236e1e5b4385ff757fcd2e117ffda56d2f
RmdGroupProjects <uramudu666@gmail.com>
2023-09-08 10:08:54 +0530

    "Code Update"

--- geturl.py
+++ geturl.py
@@ -1,6 +1,9 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 drivers=webdriver.Chrome(executable_path=r"C:\Users\ram\Desktop\Drivers\chromedriver.exe")
 drivers.maximize_window()
 drivers.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+time.sleep(6)
 
