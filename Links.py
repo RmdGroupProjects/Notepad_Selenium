39602ec03973bec1a1f7f0de75c0af52abf69e3a
RmdGroupProjects <uramudu666@gmail.com>
2023-09-11 14:02:22 +0530

    Links

---
+++ Links.py
@@ -0,0 +1,11 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+drivers=webdriver.Chrome(executable_path=r"C:\Users\ram\Desktop\Drivers\chromedriver.exe")
+drivers.maximize_window()
+drivers.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+drivers.implicitly_wait(6)
+T=drivers.find_element(By.TAG_NAME,"a")
+print(T)
+time.sleep(6)
